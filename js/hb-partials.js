b128f4a5bf85150b59763ce8cf739685f9b914ab
mikekoser <mikekoser@gmail.com>
2015-07-10 10:24:47 -0400

    Fixed issue where the response was binding the payload data not the response.

--- js/hb-partials.js
+++ js/hb-partials.js
@@ -23,6 +23,12 @@ Handlebars.partials['p-action'] = template({"1":function(depth0,helpers,partials
   return "            <tr>\r\n"
     + ((stack1 = this.invokePartial(partials['p-payload'],depth0,{"name":"p-payload","data":data,"indent":"                ","helpers":helpers,"partials":partials})) != null ? stack1 : "")
     + "            </tr>\r\n";
+},"3":function(depth0,helpers,partials,data) {
+    var stack1;
+
+  return "            <tr>\r\n"
+    + ((stack1 = this.invokePartial(partials['p-response'],depth0,{"name":"p-response","data":data,"indent":"                ","helpers":helpers,"partials":partials})) != null ? stack1 : "")
+    + "            </tr>\r\n";
 },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var stack1, helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
 
@@ -37,7 +43,7 @@ Handlebars.partials['p-action'] = template({"1":function(depth0,helpers,partials
     + "</span>\r\n    <h4>Payload</h4>\r\n    <table class=\"table--bordered table--striped table--data responsive\">\r\n        <thead>\r\n            <tr><td>Name</td><td>Type</td><td>Notes</td></tr>\r\n        </thead>\r\n        <tbody>\r\n"
     + ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.Payload : depth0),{"name":"each","hash":{},"fn":this.program(1, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "")
     + "        </tbody>\r\n    </table>\r\n    <h4>Response</h4>\r\n    <table class=\"table--bordered table--striped table--data responsive\">\r\n        <thead>\r\n            <tr><td>Name</td><td>Type</td><td>Notes</td></tr>\r\n        </thead>\r\n        <tbody>\r\n"
-    + ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.Payload : depth0),{"name":"each","hash":{},"fn":this.program(1, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "")
+    + ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.Response : depth0),{"name":"each","hash":{},"fn":this.program(3, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "")
     + "        </tbody>\r\n    </table>\r\n    <h4>Test it</h4>\r\n    <button class=\"btn btn--gamma btn--secondary\">Clear</button>\r\n    <button class=\"btn btn--gamma btn--positive btn--primary\" data-endpoint=\""
     + alias3(((helper = (helper = helpers.Endpoint || (depth0 != null ? depth0.Endpoint : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"Endpoint","hash":{},"data":data}) : helper)))
     + "\" data-verb=\""
