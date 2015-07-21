edebec06fc958e4037de71ce654c4f334998cb8a
Mike Koser <mikekoser@gmail.com>
2015-07-20 21:45:38 -0400

    Added scroll to top button

--- js/apps/doc-app.js
+++ js/apps/doc-app.js
@@ -12,11 +12,14 @@
 		Router: {},
 		Data: null,
 		Settings: {
+			"amountScrolled": 300,
+			"animationDuration": 300
 		},
 		Loaded: false,
 		Boot: function () {
-			/// <summary>Initializes the Checkout process.</summary>
+			/// <summary>Initializes the doc app.</summary>
 			DocApp.Views.CategoryList.appContext = DocApp;
+			DocApp.Functions.InitScrollToTop();
 			DocApp.Collections.Categories.fetch(DocApp).then(function (data) {
 				if (data && data.Categories) {
 					DocApp.Functions.ProcessCategories(data.Categories);
@@ -35,6 +38,24 @@
 					url: host + endpoint,
 					dataType: "json",
 					data: data ? JSON.stringify(data) : null
+				});
+			},
+			InitScrollToTop: function () {
+				// Bind scroll event to toggle the "back-to-top" button
+				var $btt = $('a.back-to-top');
+				$(window).scroll(function () {
+					if ($(window).scrollTop() > DocApp.Settings.amountScrolled) {
+						$btt.fadeIn(DocApp.Settings.animationDuration);
+					} else {
+						$btt.fadeOut(DocApp.Settings.animationDuration);
+					}
+				});
+
+				$btt.click(function () {
+					$('html,body').animate({
+						scrollTop: 0
+					}, DocApp.Settings.animationDuration);
+					return false;
 				});
 			}
 		}
